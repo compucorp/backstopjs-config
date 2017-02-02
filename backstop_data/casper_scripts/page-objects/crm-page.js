de6f2d7d73322352ebd79a84d31da87101251746
Bruno Queiros <brunoofq@gmail.com>
2017-02-02 17:55:08 -0200

    email schedule action

--- backstop_data/casper_scripts/page-objects/crm-page.js
+++ backstop_data/casper_scripts/page-objects/crm-page.js
@@ -49,6 +49,17 @@ CrmPage.prototype = Object.create({
     this.waitForSelectorAndEvaluate(function (selector) {
       document.querySelector(selector).style.display = 'none';
     }, targetSelector);
+  },
+  changeTab: function(targetSelector) {
+    this.waitForSelectorAndEvaluate(function (selector) {
+      document.querySelector('.ui-tabs-active').classList.remove('ui-tabs-active', 'ui-state-active');
+      document.querySelector('.crm-tab-button[aria-labelledby="' + selector.split('#')[1] + '"]').classList.add('ui-tabs-active', 'ui-state-active');
+
+      Array.prototype.map.call(document.querySelectorAll('.ui-tabs-panel'), function(tab) {
+        return tab.style.display = 'none';
+      });
+      document.querySelector('.ui-tabs-panel[aria-labelledby="' + selector.split('#')[1] + '"]').style.display = 'block';
+    }, targetSelector);
   }
 });
 
