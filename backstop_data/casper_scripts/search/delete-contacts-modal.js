ff2a4414f66fa9dda80368f18c01b98092ac96e0
Bruno Queiros <brunoofq@gmail.com>
2017-02-02 10:57:08 -0200

    delete contacts action

---
+++ backstop_data/casper_scripts/search/delete-contacts-modal.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = function (casper, scenario, vp) {
+  var Page = require('../page-objects/form-page.js');
+  var page = new Page(casper, scenario, vp);
+
+  casper.then(function () {
+    require('./delete-contacts')(casper, scenarios, vp);
+    page.clickFirst('#popup-button');
+  });
+};
