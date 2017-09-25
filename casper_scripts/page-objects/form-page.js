5b03d59645e17f4d9c25f85684e311ac7cc1f0f8
René Olivo <minet.ws@gmail.com>
2017-09-25 12:52:10 -0400

    PCHR-2545: Update page objects to follow JS style guide

--- casper_scripts/page-objects/form-page.js
+++ casper_scripts/page-objects/form-page.js
@@ -7,6 +7,74 @@ var FormPage = function (casper, scenario, viewPort) {
 };
 
 FormPage.prototype = Object.create(CrmPage.prototype, {
+  /**
+   * Focuses on the specified select2 dropdown.
+   *
+   * @param {String} select2ContainerSelector - the css selector for the select2
+   * dropdown to focus on.
+   */
+  activateSelect2: {
+    value: function (select2ContainerSelector) {
+      this.waitForSelectorAndEvaluate(function (selector) {
+        var event = document.createEvent('Event');
+        event.initEvent('focus', true, true);
+        document.querySelector(selector).dispatchEvent(event);
+      }, select2ContainerSelector + ' input');
+    }
+  },
+
+  /**
+   * Blurs out of the specified select2 dropdown.
+   *
+   * @param {String} select2ContainerSelector - the css selector for the select2
+   * dropdown to blur out of.
+   */
+  blurSelect2: {
+    value: function (select2ContainerSelector) {
+      this.waitForSelectorAndEvaluate(function (selector) {
+        var event = document.createEvent('Event');
+        event.initEvent('blur', true, true);
+        document.querySelector(selector).dispatchEvent(event);
+      }, select2ContainerSelector + ' input');
+    }
+  },
+
+  /**
+   * Clicks on a specific option inside the currently active select2 dropdown.
+   *
+   * @param {Number} nth - the number of the option to click on. This is a zero
+   * based index.
+   */
+  clickSelect2NthOption: {
+    value: function (nth) {
+      var selector = '.select2-drop-active li:nth-of-type(' + nth + ')';
+      this.waitForSelectorAndEvaluate(function (selector) {
+        var event = document.createEvent('MouseEvent');
+        event.initMouseEvent('mouseup', true, true);
+        document.querySelector(selector).dispatchEvent(event);
+      }, selector);
+    }
+  },
+
+  /**
+   * Disables a specific select2 dropdown.
+   *
+   * @param {String} select2ContainerSelector - the select2 dropdown to disable.
+   */
+  disableSelect2: {
+    value: function (select2ContainerSelector) {
+      this.addClassToElement(
+        select2ContainerSelector,
+        'select2-container-disabled'
+      );
+    }
+  },
+
+  /**
+   * Opens the speficied select2 dropdown.
+   *
+   * @param {String} select2ContainerSelector - the select2 dropdown to open.
+   */
   openSelect2DropDown: {
     value: function (select2ContainerSelector) {
       this.waitForSelectorAndEvaluate(function (selector) {
@@ -23,47 +91,21 @@ FormPage.prototype = Object.create(CrmPage.prototype, {
       }, select2ContainerSelector + ' .select2-choice');
     }
   },
+
+  /**
+   * Opens the specified select2 dropdown of the multiple type.
+   *
+   * @param {String} select2ContainerSelector - the select2 dropdown to open.
+   */
   openSelect2MultipleDropDown: {
     value: function (select2ContainerSelector) {
       this.waitForSelectorAndEvaluate(function (selector) {
         document.querySelector(selector).click();
       }, select2ContainerSelector + ' .select2-choices');
     }
-  },
-  clickSelect2NthOption: {
-    value: function (nth) {
-      var selector = '.select2-drop-active li:nth-of-type(' + nth + ')';
-      this.waitForSelectorAndEvaluate(function (selector) {
-        var event = document.createEvent('MouseEvent');
-        event.initMouseEvent('mouseup', true, true);
-        document.querySelector(selector).dispatchEvent(event);
-      }, selector);
-    }
-  },
-  activateSelect2: {
-    value: function (select2ContainerSelector) {
-      this.waitForSelectorAndEvaluate(function (selector) {
-        var event = document.createEvent('Event');
-        event.initEvent('focus', true, true);
-        document.querySelector(selector).dispatchEvent(event);
-      }, select2ContainerSelector + ' input');
-    }
-  },
-  disableSelect2: {
-    value: function (select2ContainerSelector) {
-      this.addClassToElement(select2ContainerSelector, 'select2-container-disabled');
-    }
-  },
-  blurSelect2: {
-    value: function (select2ContainerSelector) {
-      this.waitForSelectorAndEvaluate(function (selector) {
-        var event = document.createEvent('Event');
-        event.initEvent('blur', true, true);
-        document.querySelector(selector).dispatchEvent(event);
-      }, select2ContainerSelector + ' input');
-    }
   }
 });
+
 FormPage.prototype.constructor = FormPage;
 
 module.exports = FormPage;
