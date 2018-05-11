087b91c96d08f988adccbdd430605484d762aa00
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-11 15:29:40 +0200

    CGP-1038: Fix 'Tags' scenarios

---
+++ casper_scripts/contact/add-tag.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = function (casper, scenario, vp) {
+  var Page = require('../page-objects/contact-page.js');
+  var page = new Page(casper, scenario, vp);
+
+  casper.then(function () {
+    page.clickFirst('a[href^="/civicrm/tag/edit?action=add"]');
+    this.waitWhileSelector('.blockUI.blockOverlay');
+  });
+};
