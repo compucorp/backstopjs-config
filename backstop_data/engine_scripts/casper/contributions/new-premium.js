3f94e899daa2201dd0e246520cf061df1787abbf
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-15 14:01:46 +0200

    NDI-86: Migrate contributions-menu test suite

---
+++ backstop_data/engine_scripts/casper/contributions/new-premium.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = function (casper, scenario, vp) {
+  var Page = require('../page-objects/form-page.js');
+  var page = new Page(casper, scenario, vp);
+
+  casper.then(function () {
+    page.clickFirst('#newManagePremium');
+    this.waitWhileSelector('.blockUI.blockOverlay')
+  });
+};
