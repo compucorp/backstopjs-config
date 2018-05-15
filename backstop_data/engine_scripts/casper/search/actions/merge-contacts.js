b683de897446d686b99d95644d9822f31ae4bee0
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-15 14:29:28 +0200

    NDI-86: Migrate search-actions test suite

---
+++ backstop_data/engine_scripts/casper/search/actions/merge-contacts.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function (casper, scenario, vp) {
+  var Page = require('../../page-objects/form-page.js');
+  var page = new Page(casper, scenario, vp);
+
+  casper.then(function () {
+    page.clickFirst('#_qf_Basic_refresh');
+    page.clickFirst('#mark_x_3');
+    page.clickFirst('#mark_x_199');
+    this.wait(500);
+    page.openSelect2DropDown('#s2id_task');
+    this.wait(500);
+    page.clickSelect2NthOption(17);
+    this.wait(1000);
+  });
+};
