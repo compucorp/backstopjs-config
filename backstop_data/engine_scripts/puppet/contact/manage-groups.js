ea8b3d1059d5622f6b3aee094c9ad68ec5856744
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-19 14:28:26 +0200

    NDI-86: Add and implement wait-for-editable-icon script

--- backstop_data/engine_scripts/puppet/contact/manage-groups.js
+++ backstop_data/engine_scripts/puppet/contact/manage-groups.js
@@ -2,6 +2,7 @@
 
 module.exports = async (engine, scenario, vp) => {
   await require('../common/close-notifications')(engine, scenario, vp);
+  await require('../common/wait-for-editable-icon')(engine, scenario, vp);
   await engine.waitFor('.blockUI.blockOverlay', { hidden: true });
 };
 
