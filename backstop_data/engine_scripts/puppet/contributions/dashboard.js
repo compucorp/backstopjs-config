3c36d832a28392fa8ff6f55eccb01cb9bfcd491a
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-19 07:49:11 +0200

    NDI-86: Fix contributions/dashboard script

--- backstop_data/engine_scripts/puppet/contributions/dashboard.js
+++ backstop_data/engine_scripts/puppet/contributions/dashboard.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = async (engine, scenario, vp) => {
-  await engine.waitFor('.form-layout-compressed', { visible: true });
+  await engine.waitFor('.form-layout-compressed table', { visible: true });
 };
