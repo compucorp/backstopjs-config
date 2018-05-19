e37e6a3d2040d2650621a867e264da916f661cf7
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-19 09:24:26 +0200

    NDI-86: Fix search-actions scripts

--- backstop_data/engine_scripts/puppet/search/actions/add-group.js
+++ backstop_data/engine_scripts/puppet/search/actions/add-group.js
@@ -8,7 +8,7 @@ module.exports = async (engine, scenario, vp) => {
   await engine.click('#_qf_Basic_refresh');
   await engine.waitForNavigation();
   await engine.click('#mark_x_3');
-  await engine.waitFor(500);
+  await engine.waitFor('#search-status .select2-container:not(.select2-container-disabled)');
   await page.openSelect2DropDown('#s2id_task');
   await page.clickSelect2NthOption(13);
   await engine.waitForNavigation();
