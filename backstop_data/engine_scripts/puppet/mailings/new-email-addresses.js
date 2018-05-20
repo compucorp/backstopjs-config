68e5aebebda6ac1495f76fc65fd3c9da24521995
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-20 20:10:53 +0200

    NDI-86: Fix mailing scripts

--- backstop_data/engine_scripts/puppet/mailings/new-email-addresses.js
+++ backstop_data/engine_scripts/puppet/mailings/new-email-addresses.js
@@ -3,4 +3,5 @@
 module.exports = async (engine, scenario, vp) => {
   await engine.click('.button.new-option');
   await engine.waitFor('.blockUI.blockOverlay', { hidden: true });
+  await require('../common/wait-for-wysiwyg')(engine, scenario, vp);
 };
