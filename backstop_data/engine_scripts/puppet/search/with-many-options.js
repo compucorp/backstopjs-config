3144b59a26bc3d8e51a1e0a042164a1c37d638b7
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-24 10:42:58 +0200

    NDI-87: Fix labels in with-many-options script

--- backstop_data/engine_scripts/puppet/search/with-many-options.js
+++ backstop_data/engine_scripts/puppet/search/with-many-options.js
@@ -5,7 +5,7 @@ const Page = require('../page-objects/crm-page.js');
 module.exports = async (engine, scenario, vp) => {
   const page = new Page(engine, scenario, vp);
 
-  await page.clickSelect2Option('#s2id_tag', 'Add relationship - to organization');
-  await page.clickSelect2Option('#s2id_tag', 'Add relationship - to individual');
-  await page.clickSelect2Option('#s2id_tag', 'Add activity');
+  await page.clickSelect2Option('#s2id_tag', 'Non-profit');
+  await page.clickSelect2Option('#s2id_tag', 'Major Donor');
+  await page.clickSelect2Option('#s2id_tag', 'Company');
 };
