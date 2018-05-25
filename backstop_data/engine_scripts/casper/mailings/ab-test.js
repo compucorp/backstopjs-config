2a3096ecf72d769c906fce9df6ee36f72e5ecbb6
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-05-25 09:58:44 +0200

    NDI-87: Remove chromy and casper scripts

--- backstop_data/engine_scripts/casper/mailings/ab-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = function (casper, scenario, vp) {
-  var Page = require('../page-objects/crm-page.js');
-  var page = new Page(casper, scenario, vp);
-
-  casper.then(function () {
-    this.waitForSelector('.crm-wizard');
-  });
-};
--- backstop_data/engine_scripts/casper/search/find-activities.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = function (casper, scenario, vp) {
-  var Page = require('../page-objects/form-page.js');
-  var page = new Page(casper, scenario, vp);
-
-  casper.then(function () {
-    page.clickFirst('#_qf_Search_refresh-botton');
-  });
-};
