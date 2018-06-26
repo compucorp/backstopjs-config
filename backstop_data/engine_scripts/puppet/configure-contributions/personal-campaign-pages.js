8244d91c97baa3752ca332dfa29ec72a27f71949
Swastik Pareek <swastik1608@gmail.com>
2018-06-26 16:27:31 +0530

    SHOR-37: Resets pointer-events styles from links having disabled class - Shoreditch specific fix

--- backstop_data/engine_scripts/puppet/configure-contributions/personal-campaign-pages.js
+++ backstop_data/engine_scripts/puppet/configure-contributions/personal-campaign-pages.js
@@ -6,6 +6,11 @@ module.exports = async (engine, scenario, viewPort) => {
   const page = new Page(engine, scenario, viewPort);
 
   await engine.click('.crm-contribution-page-configure-actions .crm-hover-button');
+  // Enabling the pointer events for the link which are disabled 
+  // Reason - Since shoreditch uses boostrap button styles and adds pointer-events none to links with 'disabled' class. 
+  await engine.addStyleTag({
+    'content': 'a.disabled.crm-hover-button { pointer-events: auto !important;}'
+  });  
   await page.clickAndWaitForNavigation('a[title="Personal Campaign Pages"]');
   await engine.waitForSelector('.CRM_PCP_Form_Contribute');
   await page.enableCheckbox('input#pcp_active');
--- backstop_data/engine_scripts/puppet/configure-contributions/premiums.js
+++ backstop_data/engine_scripts/puppet/configure-contributions/premiums.js
@@ -6,6 +6,11 @@ module.exports = async (engine, scenario, viewPort) => {
   const page = new Page(engine, scenario, viewPort);
 
   await engine.click('.crm-contribution-page-configure-actions .crm-hover-button');
+  // Enabling the pointer events for the link which are disabled 
+  // Reason - Since shoreditch uses boostrap button styles and adds pointer-events none to links with 'disabled' class. 
+  await engine.addStyleTag({
+    'content': 'a.disabled.crm-hover-button { pointer-events: auto !important;}'
+  });  
   await page.clickAndWaitForNavigation('a[title="Premiums"]');
   await engine.waitForSelector('.CRM_Contribute_Form_ContributionPage_Premium');
   await page.enableCheckbox('input#premiums_active');
