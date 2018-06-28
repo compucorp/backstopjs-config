31f9806342f211e23c2b02371dfd244f1419ecb1
Swastik Pareek <swastik1608@gmail.com>
2018-06-28 20:03:51 +0530

    SHOR-40: Update priceset fields screens with latest code

--- backstop_data/engine_scripts/puppet/price-set/delete-price-field-modal.js
+++ backstop_data/engine_scripts/puppet/price-set/delete-price-field-modal.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const Page = require('../page-objects/crm-page.js');
+
 module.exports = async (engine, scenario, viewport) => {
+  const page = new Page(engine, scenario, viewport);
+
   await require('./view-and-edit-price-fields')(engine, scenario, viewport);
   await engine.click('.crm-entity span.crm-hover-button');
-  await engine.click('a[title="Delete Price"]');
-  await engine.waitFor('.CRM_Price_Form_DeleteField', { visible: true });
+  await page.clickAndWaitForModal('a[title="Delete Price"]');
 };
--- backstop_data/engine_scripts/puppet/price-set/preview-price-field-modal.js
+++ backstop_data/engine_scripts/puppet/price-set/preview-price-field-modal.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const Page = require('../page-objects/crm-page.js');
+
 module.exports = async (engine, scenario, viewport) => {
+  const page = new Page(engine, scenario, viewport);
+
   await require('./view-and-edit-price-fields')(engine, scenario, viewport);
-  await engine.click('a[title="Preview Price"]');
-  await engine.waitForSelector('.CRM_Price_Form_Preview');
+  await page.clickAndWaitForModal('a[title="Preview Price"]');
 }
