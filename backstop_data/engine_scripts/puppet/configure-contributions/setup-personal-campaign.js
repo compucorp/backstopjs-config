c787d5be745fbaf1ce1acadab43b75791fc66838
Swastik Pareek <swastik1608@gmail.com>
2018-06-28 16:51:09 +0530

    SHOR-37: PR-feedback - Add hardcoded url for setup perosnal campaign page

--- backstop_data/engine_scripts/puppet/configure-contributions/setup-personal-campaign.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = async (engine, scenario, viewPort) => {
-  await require('./personal-campaign-pages')(engine, scenario, viewPort);
-
-  const title = await engine.evaluate(el => el.innerHTML, await engine.$('.crm-contribution-contributionpage-pcp-form-block-link_text span.description em'));
-  await engine.goto(title);
-}
