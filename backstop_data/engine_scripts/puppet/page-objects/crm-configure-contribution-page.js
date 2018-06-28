1d5347474397c7cb9c23900fc6f217550381e25a
Swastik Pareek <swastik1608@gmail.com>
2018-06-28 16:56:15 +0530

    SHOR-37: PR-feedback - Linting issue fix

--- backstop_data/engine_scripts/puppet/page-objects/crm-configure-contribution-page.js
+++ backstop_data/engine_scripts/puppet/page-objects/crm-configure-contribution-page.js
@@ -2,10 +2,10 @@
 
 const CRMPage = require('./crm-page.js');
 
-module.exports = class ConfigureContributionsPage extends CRMPage{
+module.exports = class ConfigureContributionsPage extends CRMPage {
 
   constructor(engine, scenario, viewPort) {
-    super(engine, scenario, viewPort);
+    super(...arguments);
     this.engine = engine;
 
     enablePointerEvents.call(this);
@@ -13,7 +13,7 @@ module.exports = class ConfigureContributionsPage extends CRMPage{
 }
 
 /**
- * Private function - Override pointer-events none property to auto for each disabled a link on the page.
+ * Override pointer-events none property to auto for each disabled a link on the page.
  * Reason - Since shoreditch uses boostrap button styles and adds pointer-events none to links with 'disabled' class. 
  */
 async function enablePointerEvents() {
