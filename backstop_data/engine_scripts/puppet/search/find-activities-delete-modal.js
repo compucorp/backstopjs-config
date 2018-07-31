86b2842cebdf79afb26de3ab412eab865d93623a
Swastik Pareek <swastik1608@gmail.com>
2018-07-31 14:14:30 +0530

    SHOR-52: PR feedback - Fix code linting

--- backstop_data/engine_scripts/puppet/search/find-activities-delete-modal.js
+++ backstop_data/engine_scripts/puppet/search/find-activities-delete-modal.js
@@ -6,7 +6,7 @@ module.exports = async (engine, scenario, vp) => {
   const page = await Page.build(engine, scenario, vp);
 
   await require('./find-activities')(engine, scenario, vp);
-  if( !!(await engine.$('.crm-search-results span.crm-hover-button'))) {
+  if (await engine.$('.crm-search-results span.crm-hover-button')) {
     await engine.click('.crm-search-results span.crm-hover-button');
   }
   await page.clickAndWaitForModal('a[title="Delete Activity"]');
