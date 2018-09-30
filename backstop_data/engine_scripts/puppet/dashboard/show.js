75a67fbaf7e432c97acfd656b58d93edde8e1068
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-30 20:28:39 +0200

    BASW-269: Improve reliability of dashboard scenario

--- backstop_data/engine_scripts/puppet/dashboard/show.js
+++ backstop_data/engine_scripts/puppet/dashboard/show.js
@@ -1,5 +1,6 @@
 'use strict';
 
-module.exports = async (engine) => {
+module.exports = async (engine, scenario, vp) => {
+  await require('../common/wait-for-editable-icon')(engine, scenario, vp);
   await engine.waitForSelector('.blockUI.blockOverlay', { hidden: true });
 };
