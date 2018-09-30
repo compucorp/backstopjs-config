0ebe73130351acaeb1df5efa2918156049eda583
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-30 21:33:41 +0200

    BASW-269: Improve reliability of membership menu scenarios

--- backstop_data/engine_scripts/puppet/membership/search-result.js
+++ backstop_data/engine_scripts/puppet/membership/search-result.js
@@ -4,7 +4,18 @@ const Page = require('../page-objects/crm-page.js');
 
 module.exports = async (engine, scenario, vp) => {
   const page = await Page.build(engine, scenario, vp);
+  let trySubmit = true;
 
-  await require('../common/open-accordions')(engine, scenario, vp);
-  await page.clickAndWaitForNavigation('#_qf_Search_refresh');
+  // CiviCRM doesn't like when multiple searches are being submitted at the same
+  // time, causing in a fatal error. This loop tries until the search is submitted
+  // successfully
+  while (trySubmit) {
+    await page.clickAndWaitForNavigation('#_qf_Search_refresh');
+
+    if (await engine.$('.status-fatal')) {
+      await engine.goto(scenario.url);
+    } else {
+      trySubmit = false;
+    }
+  }
 };
