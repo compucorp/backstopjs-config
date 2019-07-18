2e2df24a3d90330ac8b2179407d90e24c98e86d0
Igor Pavlov <pavlov.org@gmail.com>
2019-07-18 17:25:44 +0100

    SHOR-139: Fix group belonging bug

--- backstop_data/engine_scripts/puppet/manage-groups/add-contacts-to-group-results.js
+++ backstop_data/engine_scripts/puppet/manage-groups/add-contacts-to-group-results.js
@@ -7,6 +7,6 @@ module.exports = async (engine, scenario, vp) => {
 
   await require('./add-contacts-to-group-search-form')(engine, scenario, vp);
   await engine.waitForSelector('input[name="sort_name"]', {visible: true});
-  await engine.type('input[name="sort_name"]', 'Patel');
+  await engine.type('input[name="sort_name"]', 'Adams family');
   await page.clickAndWaitForNavigation('#_qf_Basic_refresh');
 };
