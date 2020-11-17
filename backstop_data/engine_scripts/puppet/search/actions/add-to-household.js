54666215b781de4f2790aecc89c695d4652e6280
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2020-11-17 13:06:56 +0000

    CPS-378: Fix search related scenarios

--- backstop_data/engine_scripts/puppet/search/actions/add-to-household.js
+++ backstop_data/engine_scripts/puppet/search/actions/add-to-household.js
@@ -6,6 +6,6 @@ module.exports = async (engine, scenario, vp) => {
   const page = await Page.build(engine, scenario, vp);
 
   await require('./common')(page);
-  await page.clickSelect2Option('#s2id_task', 'Add relationship - to household');
+  await page.clickSelect2Option('#s2id_task', 'Add relationship - to Household');
   await engine.waitForNavigation();
 };
